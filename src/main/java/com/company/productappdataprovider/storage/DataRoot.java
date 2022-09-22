5f743b532d12d878de6817bd729ab5fb0d351e52
Kümmel Christian <c.kuemmel@microstream.one>
2022-09-22 16:44:50 +0200

    final commit

--- src/main/java/com/company/productappdataprovider/storage/DataRoot.java
+++ src/main/java/com/company/productappdataprovider/storage/DataRoot.java
@@ -1,13 +1,6 @@
 
 package com.company.productappdataprovider.storage;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.company.productappdataprovider.domain.Category;
-import com.company.productappdataprovider.domain.Product;
-
-
 /**
  * MicroStream data root. Create your data model from here.
  * 
@@ -15,27 +8,5 @@ import com.company.productappdataprovider.domain.Product;
  */
 public class DataRoot
 {
-	private final List<Product>		products	= new ArrayList<Product>();
-	private final List<Category>	categories	= new ArrayList<Category>();
-	private boolean					firstStart	= true;
-	
-	public List<Product> getProducts()
-	{
-		return products;
-	}
-	
-	public List<Category> getCategories()
-	{
-		return categories;
-	}
-	
-	public boolean isFirstStart()
-	{
-		return firstStart;
-	}
-	
-	public void setFirstStart(final boolean firstStart)
-	{
-		this.firstStart = firstStart;
-	}
+
 }
